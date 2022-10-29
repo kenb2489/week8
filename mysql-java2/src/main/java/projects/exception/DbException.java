c64dfed5317105d3e39f2d33d280927271cc2c8c
Ken Bernstein <112273610+kenb2489@users.noreply.github.com>
2022-10-29 12:27:37 -0700

    Add files via upload

---
+++ mysql-java2/src/main/java/projects/exception/DbException.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package projects.exception;
+
+/**
+ * @author kenb1
+ *
+ */
+@SuppressWarnings("serial")
+public class DbException extends RuntimeException {
+
+  /**
+   * 
+   */
+  public DbException() {    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @param message
+   */
+  public DbException(String message) {
+    super(message);    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @param cause
+   */
+  public DbException(Throwable cause) {
+    super(cause);    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @param message
+   * @param cause
+   */
+  public DbException(String message, Throwable cause) {
+    super(message, cause);
+    // TODO Auto-generated constructor stub
+  }
+
+}
---
+++ mysql-java2/src/main/java/projects/projects.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package projects;
+
+import projects.dao.DbConnection;
+
+/**
+ * @author kenb1
+ *
+ */
+public class projects {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    DbConnection.getConnection();
+  }
+
+}
